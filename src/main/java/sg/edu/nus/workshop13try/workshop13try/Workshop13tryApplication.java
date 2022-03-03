6fc072f464e29b7aac4897e942fb64697798461a
Saravanan Munusamy <saravananmunusamy@Saravanans-MacBook-Pro.local>
2022-03-03 08:32:23 +0800

    first commit

---
+++ src/main/java/sg/edu/nus/workshop13try/workshop13try/Workshop13tryApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.workshop13try.workshop13try;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Workshop13tryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Workshop13tryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/workshop13try/workshop13try/Workshop13tryApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.workshop13try.workshop13try;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Workshop13tryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
